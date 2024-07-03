19bf8e099d517eeb0934d0c161838a76178dbf82
vitkhanh <vietkhanh137@gmail.com>
2024-07-03 22:29:05 +0700

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-
 const { Telegraf } = require('telegraf');
 const express = require('express')
+const axios = require("axios");
 const app = express()
 const cron = require('node-cron');
-const {WebSocket, WebSocketServer} = require("ws")
-require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN_TELEGRAM);
-
+require('dotenv').config();
 
 app.use(express.static('static'))
 app.use(express.json());
 
 
-
-
-
 // const coin = 'lrcusdt';
 
 // const ws = new WebSocket(`wss://fstream.binance.com/ws/${coin}@trade`);
-
-// const job = new CronJob(
-// 	'00 01 * * * *', // cronTime
-// 	function () {
-//         console.log(123123);
-//         botTele.telegram.sendMessage(1766285817, "ping" , {})
-// 	}, // onTick
-// 	null, // onComplete
-// 	true, // start
-// 	'Asia/Ho_Chi_Minh' // timeZone
-// );
-
-// job.start()
 
 // ws.on('message', (data) => {
 //     if (data) {
@@ -51,9 +33,18 @@ app.use(express.json());
 
 //   ws.send('something');
 // });
-
+// 00 7 * * 0-6
 cron.schedule('* * * * *', () => {
-  bot.telegram.sendMessage(1766285817, "ping" , {})
+  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=loopring&vs_currencies=usd`)
+  .then(response => {
+    console.log(response.data)
+    const message = `Hello, today the Loopring price is ${response.data.loopring.usd}USD`
+    bot.telegram.sendMessage("1766285817", message, {
+    })
+  })
+}, {
+  scheduled: true,
+  timezone: "Asia/Ho_Chi_Minh"
 });
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
