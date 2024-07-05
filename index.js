bfad3348e7959a9650e4300945e5242bdd29762c
vitkhanh <vietkhanh137@gmail.com>
2024-07-05 10:00:02 +0700

    first commit

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ const app = express()
 const cron = require('node-cron');
 require('dotenv').config();
 
+
+const PORT = process.env.PORT || 3000
 const bot = new Telegraf(process.env.BOT_TOKEN_TELEGRAM);
 app.use(express.static('static'))
 app.use(express.json());
 
-cron.schedule('00 7 * * *', () => {
-  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=loopring&vs_currencies=usd`)
-  .then(response => {
-    const message = `Hello, today the Loopring price is ${response.data.loopring.usd}USD`
-      bot.telegram.sendMessage("1766285817", message, {})
-  })
+// cron.schedule('00 7 * * *', () => {
+//   axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=loopring&vs_currencies=usd`)
+//   .then(response => {
+//     const message = `Hello, today the Loopring price is ${response.data.loopring.usd}USD`
+//     bot.telegram.sendMessage("1766285817", message, {})
+//   })
+// }, {
+//   scheduled: true,
+//   timezone: "Asia/Ho_Chi_Minh"
+// });
+
+cron.schedule('* * * * *', () => {
+  const message = `123`
+  bot.telegram.sendMessage("1766285817", message, {})
 }, {
   scheduled: true,
   timezone: "Asia/Ho_Chi_Minh"
@@ -22,6 +32,6 @@ cron.schedule('00 7 * * *', () => {
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
   
 bot.launch()
