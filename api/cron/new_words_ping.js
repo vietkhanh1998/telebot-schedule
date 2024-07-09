cdc9945475feee61407dd25641767931ceacbd85
vitkhanh <vietkhanh137@gmail.com>
2024-07-09 11:52:47 +0700

    update

--- api/cron/new_words_ping.js
+++ api/cron/new_words_ping.js
@@ -1,4 +1,5 @@
 const pingNewWords = (chatId, schedule, bot) => {
+    bot.telegram.sendMessage(chatId, `Bot running....!`, {});
     const wordsList = [
         ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"],
         ["mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli fruit", "vanilla"],
